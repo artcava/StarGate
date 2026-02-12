a37baf9c99c246a3fb06fa281d89eba9196f8a9c
Marco Cavallo <cavallo.marco@gmail.com>
2026-02-12 12:25:14 +0100

    fix: add missing using Microsoft.Extensions.Logging to all files
    Fixes remaining 8 compilation errors
    
    Relates to #19

--- src/StarGate.Infrastructure/Persistence/MongoDbHealthCheck.cs
+++ src/StarGate.Infrastructure/Persistence/MongoDbHealthCheck.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
--- src/StarGate.Infrastructure/Persistence/MongoDbIndexCreationService.cs
+++ src/StarGate.Infrastructure/Persistence/MongoDbIndexCreationService.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 
 namespace StarGate.Infrastructure.Persistence;
