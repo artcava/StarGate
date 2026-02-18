c91930137954fc020af10b3a0db1a7054155030d
Marco Cavallo <cavallo.marco@gmail.com>
2026-02-18 09:41:43 +0100

    fix: remove non-existent extension methods from Program.cs

--- src/StarGate.Server/Program.cs
+++ src/StarGate.Server/Program.cs
@@ -1,12 +1,6 @@
-using StarGate.Infrastructure.Extensions;
-using StarGate.Core.Extensions;
 using StarGate.Server.Workers;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
-
-// Add services
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddDomain();
 
 // Register background worker
 builder.Services.AddHostedService<ProcessWorker>();
