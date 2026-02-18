f2c88831d671e92b8c52c5fc57200226eca7447b
Marco Cavallo <cavallo.marco@gmail.com>
2026-02-18 09:29:26 +0100

    chore: register ProcessWorker as hosted service

--- src/StarGate.Server/Program.cs
+++ src/StarGate.Server/Program.cs
@@ -1,4 +1,15 @@
+using StarGate.Infrastructure.Extensions;
+using StarGate.Core.Extensions;
+using StarGate.Server.Workers;
+
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+// Add services
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddDomain();
+
+// Register background worker
+builder.Services.AddHostedService<ProcessWorker>();
 
 IHost host = builder.Build();
 host.Run();
