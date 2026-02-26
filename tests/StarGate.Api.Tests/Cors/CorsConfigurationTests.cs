c646295cd962432e5b9551a38bd3e2691cf96082
Marco Cavallo <mcavallo@welol.it>
2026-02-26 15:54:52 +0100

    fix errors

--- tests/StarGate.Api.Tests/Cors/CorsConfigurationTests.cs
+++ tests/StarGate.Api.Tests/Cors/CorsConfigurationTests.cs
@@ -1,5 +1,3 @@
-namespace StarGate.Api.Tests.Cors;
-
 using FluentAssertions;
 using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
@@ -9,7 +7,8 @@ using Microsoft.Extensions.Hosting;
 using Moq;
 using StarGate.Api.Configuration;
 using StarGate.Api.Extensions;
-using Xunit;
+
+namespace StarGate.Api.Tests.Cors;
 
 public class CorsConfigurationTests
 {
